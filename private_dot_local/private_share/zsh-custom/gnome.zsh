dd4b797ab8c3478dbb702d8c6a761caec6989be4
erdii <me@erdii.engineering>
2021-02-18 13:22:26 +0100

    automatically apply synced gnome settings when they change

--- private_dot_local/private_share/zsh-custom/gnome.zsh
+++ private_dot_local/private_share/zsh-custom/gnome.zsh
@@ -1,2 +1,2 @@
-alias gnome-settings-backup="dconf dump / > \"$HOME/.config/dconf/settings.ini\""
-alias gnome-settings-restore="dconf load / < \"$HOME/.config/dconf/settings.ini\""
+alias gnome-settings-backup="dconf dump / > \"$(chezmoi source-path)/ignored_files/dconf.ini\""
+alias gnome-settings-restore="dconf load / < \"$(chezmoi source-path)/ignored_files/dconf.ini\""
