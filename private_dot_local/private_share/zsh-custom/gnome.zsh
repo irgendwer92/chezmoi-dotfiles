b2f7067a3ed5ab41ee1290cb632e87680c25e604
erdii <me@erdii.engineering>
2021-02-19 10:57:17 +0100

    clean up ignored files tree

--- private_dot_local/private_share/zsh-custom/gnome.zsh
+++ private_dot_local/private_share/zsh-custom/gnome.zsh
@@ -1,2 +1,2 @@
-alias gnome-settings-backup="dconf dump / > \"$(chezmoi source-path)/ignored_files/dconf.ini\""
-alias gnome-settings-restore="dconf load / < \"$(chezmoi source-path)/ignored_files/dconf.ini\""
+alias gnome-settings-backup='dconf dump / > "$(chezmoi source-path)/ignored/included/dconf.ini"'
+alias gnome-settings-restore='dconf load / < "$(chezmoi source-path)/ignored/included/dconf.ini"'
