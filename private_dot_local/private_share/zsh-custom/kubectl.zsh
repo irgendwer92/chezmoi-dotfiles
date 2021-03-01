2ddb1c7272a2f3d659d04d247e1a6ea4be914ff2
erdii <me@erdii.engineering>
2021-03-01 15:51:08 +0100

    Update private_dot_local/private_share/zsh-custom/kubectl.zsh

--- private_dot_local/private_share/zsh-custom/kubectl.zsh
+++ private_dot_local/private_share/zsh-custom/kubectl.zsh
@@ -1,11 +1,25 @@
 if [[ -f $(command -v kubectl) ]]; then
   function kubectl-apply-secret() {
-    kubectl create secret generic "$1" \
+    local name="$1"
+    local filepath="$2"
+
+    if [[ -z "$name" || -z "$filepath" ]]; then
+      echo "kubectl-apply-secret secret-name file-path [kubectl arguments...]"
+      echo "examples:"
+      echo "\tkubectl-apply-secret name path/to/folder/or/file"
+      echo "\tkubectl-apply-secret name path/to/folder/or/file -n foo"
+      return
+    fi
+    shift
+    shift
+
+    kubectl create secret generic "$name" \
       --save-config --dry-run=client \
-      --from-file="$2" \
-      -o yaml | kubectl apply -f -
+      --from-file="$filepath" \
+      -o yaml \
+      "$@" | kubectl apply -f -
   }
 
-  alias kw="watch kubectl"
   alias kas="kubectl-apply-secret"
+  alias kw="watch kubectl"
 fi
